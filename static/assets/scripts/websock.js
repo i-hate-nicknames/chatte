febd27461412ae220ecc410821e1cfe27d2406d6
nvm <Never M>
2020-05-12 13:23:38 +0300

    Send text from the browser

--- static/assets/scripts/websock.js
+++ static/assets/scripts/websock.js
@@ -1,4 +1,42 @@
-let sock = new WebSocket('ws://localhost:8080/websock');
-sock.onopen = function (event) {
-    sock.send("hello server");
+const wsAddr = 'ws://localhost:8080/websock';
+
+let state = {
+    sock: null,
+    sendBtn: null,
+    messageLog: null,
+    userInputArea: null,
+    initialized: false,
+};
+
+function InitSocket(state) {
+    let sock = new WebSocket(wsAddr);
+    sock.onopen = function (event) {
+        state.initialized = true;
+    }
+    state.sock = sock;
 }
+
+function initDom() {
+    state.sendBtn = document.getElementById("send");
+    state.messageLog = document.getElementById("message-log");
+    state.userInputArea = document.getElementById("user-input");
+    state.sendBtn.removeAttribute("disabled");
+    state.sendBtn.onclick = sendMessage;
+}
+
+function sendMessage() {
+    let text = state.userInputArea.value;
+    if (!state.initialized || text === "") {
+        return;
+    }
+    console.log("sending " + text);
+    state.sock.send(text);
+    state.userInputArea.value = "";
+}
+
+function StartApp() {
+    initDom();
+    InitSocket(state)
+}
+
+StartApp();
