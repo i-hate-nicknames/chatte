63401252c19a54d34459a591ae00d5aa4786d8a5
nvm <nevermn@protonmail.com>
2020-07-13 19:35:40 +0300

    Implement sending using events

--- web/scripts/effector.js
+++ web/scripts/effector.js
@@ -2,6 +2,8 @@ import { createStore, createEffect, createEvent } from 'effector'
 
 const makeLogger = text => data => { console.log(`${text}: ${data}`)}
 
+// setting up connection and receiving
+
 const wsAddr = 'ws://127.0.0.1:8080/websock';
 
 const onMessage = createEvent('new message')
@@ -12,9 +14,10 @@ sock.onopen = () => {
     sock.onclose = () => { console.log("closing") }
 }
 
-const onMessageParse = onMessage.map(msg => JSON.parse(msg.data))
+// todo: use when the server will actually encode the messages
+// const onMessageParse = onMessage.map(msg => JSON.parse(msg.data))
+// onMessageParse.watch(makeLogger("Message from server"))
 
-onMessageParse.watch(makeLogger("Message from server"))
 
 const $msgs = createStore([])
 
@@ -24,8 +27,22 @@ const $msgCount = $msgs.map(msgs => msgs.length)
 
 $msgCount.watch(makeLogger("Msg count"))
 
+// sending
+
 const sendText = createEvent('send')
 
 const sendSerialized = sendText.map(text => JSON.stringify({type: "PUBLIC", payload: text}))
 
 sendSerialized.watch(makeLogger("Sending"))
+
+sendSerialized.watch(data => {
+    sock.send(data)
+})
+
+const btnSend = document.getElementById("send")
+const userInputArea = document.getElementById("user-input");
+btnSend.onclick = () => {
+    const text = userInputArea.value;
+    sendText(text)
+    userInputArea.value = "";
+}
